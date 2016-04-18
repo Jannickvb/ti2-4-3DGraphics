715f1d40da78f168ba466cba3b124cdc6e6b961a
Jannick van Ballegooijen <jvbdesignz@gmail.com>
2016-04-18 22:57:34 +0200

    added base models

---
+++ wk1_Rasterizer_unfinished/Rasterizer/assets/models/Object3D.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rasterizer.assets.models
+{
+    abstract class Object3D
+    {
+
+        public List<Vector3> vertices { get; }
+        public List<List<int>> polygons { get; }
+        public Matrix translation { get; }
+
+        public Object3D()
+        {
+
+        }
+
+        public void Render(Graphics g)
+        {
+
+        }
+    }
+}
