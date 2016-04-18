b45f27108632beb38c9fc25c3800f1c7630bcad6
Remco <Remco.boys@gmail.com>
2016-04-18 13:09:22 +0200

    working code

--- wk1_Rasterizer_unfinished/Rasterizer/Form1.cs
+++ wk1_Rasterizer_unfinished/Rasterizer/Form1.cs
@@ -19,16 +19,36 @@ namespace Rasterizer
 		}
 		List<Vector3> vertices = new List<Vector3>();
 		List<List<int>> polygons = new List<List<int>>();
-		float rotation;
+		float rotation = 0;
 
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			//initialise vertices & polygons here
-		}
+            //initialise vertices & polygons here
+            vertices.Add(new Vector3(-1f, -1f, -1f));
+            vertices.Add(new Vector3( 1f, -1f, -1f));
+            vertices.Add(new Vector3( 1f,  1f, -1f));
+            vertices.Add(new Vector3(-1f,  1f, -1f));
+            //vertices.Add(new Vector3() { x = -1f, y = -1f, z = -1f });
+            //vertices.Add(new Vector3() { x =  1f, y = -1f, z = -1f });
+            //vertices.Add(new Vector3() { x =  1f, y =  1f, z = -1f });
+            //vertices.Add(new Vector3() { x = -1f, y =  1f, z = -1f });
+
+            vertices.Add(new Vector3(-1f, -1f, 1f));
+            vertices.Add(new Vector3( 1f, -1f, 1f));
+            vertices.Add(new Vector3( 1f,  1f, 1f));
+            vertices.Add(new Vector3(-1f,  1f, 1f));
+            //vertices.Add(new Vector3() { x = -1f, y = -1f, z = 1f });
+            //vertices.Add(new Vector3() { x =  1f, y = -1f, z = 1f });
+            //vertices.Add(new Vector3() { x =  1f, y =  1f, z = 1f });
+            //vertices.Add(new Vector3() { x = -1f, y =  1f, z = 1f });
+
+            polygons.Add(new List<int> { 1,2,3,4});
+        }
 
 		private void timer1_Tick(object sender, EventArgs e)
 		{
+            rotation += 0.01f;
 			Invalidate();
 		}
 
@@ -39,7 +59,24 @@ namespace Rasterizer
 			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 			Pen pen = new Pen(Color.Black, 2);
 
-			g.DrawRectangle(pen, 50, 50, 10, 10);
+            
+
+            Matrix perspective = Matrix.perspective((float)Math.PI/2, 1.0f ,.1f,10f);
+            Matrix translation = Matrix.translate(new Vector3(0, 0, 0));
+            Matrix rotationMatrix = Matrix.rotation(rotation, new Vector3(0, 1, 0));
+            float schermPositieX = 0, schermPositieY = 0;
+
+            foreach (Vector3 v in vertices)
+            {
+                Vector3 v1 = perspective * translation* rotationMatrix * v;
+//                Console.WriteLine($"Width: {Width},Height: {Height}");
+                schermPositieX = Width / 2 + v1.x / v1.z * Width / 2;
+                schermPositieY = Height / 2 + v1.y / v1.z * Height / 2;
+
+                g.DrawRectangle(pen, (int)schermPositieX-5, (int)schermPositieY-5, 10, 10);
+            }
+
+			//g.DrawRectangle(pen, 50, 50, 10, 10);
 		}
 
 
--- wk1_Rasterizer_unfinished/Rasterizer/Matrix.cs
+++ wk1_Rasterizer_unfinished/Rasterizer/Matrix.cs
@@ -28,27 +28,74 @@ namespace Rasterizer
 
 		public static Matrix perspective(float fov, float aspect, float zNear, float zFar)
 		{
-			throw new NotImplementedException();
+            Matrix perspective = identity();
+
+            perspective.data[0, 0] = (float)(Math.Tan(fov / 2) / aspect);
+            perspective.data[1, 1] = (float)(Math.Tan(fov / 2));
+            perspective.data[2, 2] = zFar / (zFar - zNear);
+            perspective.data[2, 3] = (zNear*zFar) * (zFar-zNear);
+            perspective.data[3, 2] = -1;
+
+            return perspective;
 		}
 
 		public static Matrix rotation(float angle, Vector3 axis)
 		{
-			throw new NotImplementedException();
+            Matrix rotation = identity();
+            float c, s;
+            c = (float)Math.Cos(angle);
+            s = (float)Math.Sin(angle);
+
+            rotation.data[0, 0] = (float)(Math.Pow(axis.x,2)*(1-c)+c);
+            rotation.data[0, 1] = (axis.x*axis.y*(1-c)-axis.z*s);
+            rotation.data[0, 2] = (axis.x*axis.z*(1-c)+axis.y*s);
+
+            rotation.data[1, 0] = (axis.x*axis.y*(1-c)+axis.z*s);
+            rotation.data[1, 1] = (float)(Math.Pow(axis.y,2)*(1-c)+c);
+            rotation.data[1, 2] = (axis.y*axis.z*(1-c)-axis.x*s);
+
+            rotation.data[2, 0] = (axis.x*axis.z*(1-c)-axis.y*s);
+            rotation.data[2, 1] = (axis.y*axis.z*(1-c)+axis.x*s);
+            rotation.data[2, 2] = (float)(Math.Pow(axis.z,2)*(1-c)+c);
+
+            return rotation;
 		}
 		public static Matrix translate(Vector3 offset)
 		{
-			throw new NotImplementedException();
+            Matrix translation = identity();
+
+            translation.data[0, 3] = offset.x;
+            translation.data[1, 3] = offset.y;
+            translation.data[2, 3] = offset.z;
+
+            return translation;
 		}
 
 
 
-		public static Vector3 operator * (Matrix mat, Vector3 vec)
-		{
-			throw new NotImplementedException();
-		}
+        public static Vector3 operator *(Matrix mat, Vector3 vec)
+        {
+            float x, y, z;
+            x = (mat.data[0, 0] * vec.x) + (mat.data[0, 1] * vec.y) + (mat.data[0, 2] * vec.z) + mat.data[0, 3];
+            y = (mat.data[1, 0] * vec.x) + (mat.data[1, 1] * vec.y) + (mat.data[1, 2] * vec.z) + mat.data[1, 3];
+            z = (mat.data[2, 0] * vec.x) + (mat.data[2, 1] * vec.y) + (mat.data[2, 2] * vec.z) + mat.data[2, 3];
+
+            return new Vector3( x, y, z );
+        }
 		public static Matrix operator * (Matrix mat1, Matrix mat2)
 		{
-			throw new NotImplementedException();
-		}
+            Matrix result = Matrix.identity();
+            result.data = new float[mat1.data.GetLength(0), mat2.data.GetLength(1)];
+            for (int i = 0; i < result.data.GetLength(0); i++)
+            {
+                for (int j = 0; j < result.data.GetLength(1); j++)
+                {
+                    result.data[i, j] = 0;
+                    for (int k = 0; k < mat1.data.GetLength(1); k++)
+                        result.data[i, j] = result.data[i, j] + mat1.data[i, k] * mat2.data[k, j];
+                }
+            }
+            return result;
+        }
 	}
 }
