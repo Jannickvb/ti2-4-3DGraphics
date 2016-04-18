a8894a8b846ca199bef3c275171b101a0b34e6d0
Dofensmirtsz <dofensmirtsz@gmail.com>
2016-04-18 17:11:52 +0200

    Kegel + Cilinder + Begin Bol gemaakt

--- wk1_Rasterizer_unfinished/Rasterizer/Form1.cs
+++ wk1_Rasterizer_unfinished/Rasterizer/Form1.cs
@@ -24,29 +24,10 @@ namespace Rasterizer
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-            //initialise vertices & polygons here
-            vertices.Add(new Vector3(-1f, -1f, -1f));
-            vertices.Add(new Vector3( 1f, -1f, -1f));
-            vertices.Add(new Vector3( 1f,  1f, -1f));
-            vertices.Add(new Vector3(-1f,  1f, -1f));
-            //vertices.Add(new Vector3() { x = -1f, y = -1f, z = -1f });
-            //vertices.Add(new Vector3() { x =  1f, y = -1f, z = -1f });
-            //vertices.Add(new Vector3() { x =  1f, y =  1f, z = -1f });
-            //vertices.Add(new Vector3() { x = -1f, y =  1f, z = -1f });
-
-            vertices.Add(new Vector3(-1f, -1f, 1f));
-            vertices.Add(new Vector3( 1f, -1f, 1f));
-            vertices.Add(new Vector3( 1f,  1f, 1f));
-            vertices.Add(new Vector3(-1f,  1f, 1f));
-            //vertices.Add(new Vector3() { x = -1f, y = -1f, z = 1f });
-            //vertices.Add(new Vector3() { x =  1f, y = -1f, z = 1f });
-            //vertices.Add(new Vector3() { x =  1f, y =  1f, z = 1f });
-            //vertices.Add(new Vector3() { x = -1f, y =  1f, z = 1f });
-
-            polygons.Add(new List<int> { 0,1,2,3});
-            polygons.Add(new List<int> { 4, 5, 6, 7});
-            polygons.Add(new List<int> { 0, 1, 5, 4 });
-            polygons.Add(new List<int> { 2, 3, 7, 6 });
+            //Kubus();
+            //Kegel();
+            //Cilinder();
+            Bol();
         }
 
 		private void timer1_Tick(object sender, EventArgs e)
@@ -62,11 +43,11 @@ namespace Rasterizer
 			g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 			Pen pen = new Pen(Color.Black, 2);
 
-            
+
 
             Matrix perspective = Matrix.perspective((float)Math.PI/2, (float)Width/Height,.1f,50);
-            Matrix translation = Matrix.translate(new Vector3(0, 0, -5.0f));
-            Matrix rotationMatrix = Matrix.rotation(rotation, new Vector3(0, 1, 0));
+            Matrix translation = Matrix.translate(new Vector3(0, 0, -5f));
+            Matrix rotationMatrix = Matrix.rotation(rotation, new Vector3(1f, 0, 0));
             float schermPositieX = 0, schermPositieY = 0;
 
             foreach (Vector3 v in vertices)
@@ -95,14 +76,124 @@ namespace Rasterizer
 
                     g.DrawLine(pen, screenP, lastPoint);
                     lastPoint = screenP;
+
+
                 }
             }
-			//g.DrawRectangle(pen, 50, 50, 10, 10);
-		}
+        }
 
 
 
 
+        private void Kubus()
+        {
+            vertices.Add(new Vector3(-1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(1.0f, 1.0f, -1.0f));
+            vertices.Add(new Vector3(-1.0f, 1.0f, -1.0f));
 
-	}
+            vertices.Add(new Vector3(-1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(1.0f, 1.0f, 1.0f));
+            vertices.Add(new Vector3(-1.0f, 1.0f, 1.0f));
+
+            polygons.Add(new List<int> { 0, 1, 2, 3 });
+            polygons.Add(new List<int> { 4, 5, 6, 7 });
+            polygons.Add(new List<int> { 0, 1, 5, 4 });
+            polygons.Add(new List<int> { 2, 3, 7, 6 });
+        }
+
+        private void Kegel()
+        {
+            //onderkant
+            vertices.Add(new Vector3(1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(1.5f, -1.0f, 0f));
+            vertices.Add(new Vector3(1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(0f, -1.0f, -1.5f));
+            vertices.Add(new Vector3(-1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(-1.5f, -1.0f, 0f));
+            vertices.Add(new Vector3(-1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(0f, -1.0f, 1.5f));
+
+            //bovenste vertix
+            vertices.Add(new Vector3(0f, 2f, 0f));
+
+            //lijntjes vormen driehoeken
+            polygons.Add(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+            polygons.Add(new List<int> { 0, 4, 8 });
+            polygons.Add(new List<int> { 1, 5, 8 });
+            polygons.Add(new List<int> { 2, 6, 8 });
+            polygons.Add(new List<int> { 3, 7, 8 });
+        }
+
+        private void Cilinder()
+        {
+            //bovenste rondje
+            vertices.Add(new Vector3(1.0f, 2f, 1.0f));
+            vertices.Add(new Vector3(1.5f, 2f, 0f));
+            vertices.Add(new Vector3(1.0f, 2f, -1.0f));
+            vertices.Add(new Vector3(0f, 2f, -1.5f));
+            vertices.Add(new Vector3(-1.0f, 2f, -1.0f));
+            vertices.Add(new Vector3(-1.5f, 2f, 0f));
+            vertices.Add(new Vector3(-1.0f, 2f, 1.0f));
+            vertices.Add(new Vector3(0f, 2f, 1.5f));
+            
+            polygons.Add(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+
+            //onderste rondje
+            vertices.Add(new Vector3(1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(1.5f, -1.0f, 0f));
+            vertices.Add(new Vector3(1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(0f, -1.0f, -1.5f));
+            vertices.Add(new Vector3(-1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(-1.5f, -1.0f, 0f));
+            vertices.Add(new Vector3(-1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(0f, -1.0f, 1.5f));
+            polygons.Add(new List<int> { 8, 9, 10, 11, 12, 13, 14, 15 });
+
+            //lijnen tussen onder en boven     //dit is voor de streepjes in het midden, lijstjes vormen vierkanten
+            polygons.Add(new List<int> { 0, 8, 12, 4 });
+            polygons.Add(new List<int> { 1, 9, 13, 5 });
+            polygons.Add(new List<int> { 2, 10, 14, 6 });
+            polygons.Add(new List<int> { 3, 11, 15, 7 });
+        }
+
+        private void Bol()
+        {
+            //bovenste rondje
+            vertices.Add(new Vector3(1.0f, 2f, 1.0f));
+            vertices.Add(new Vector3(1.5f, 2f, 0f));
+            vertices.Add(new Vector3(1.0f, 2f, -1.0f));
+            vertices.Add(new Vector3(0f, 2f, -1.5f));
+            vertices.Add(new Vector3(-1.0f, 2f, -1.0f));
+            vertices.Add(new Vector3(-1.5f, 2f, 0f));
+            vertices.Add(new Vector3(-1.0f, 2f, 1.0f));
+            vertices.Add(new Vector3(0f, 2f, 1.5f));
+
+            polygons.Add(new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 });
+
+            //onderste rondje
+            vertices.Add(new Vector3(1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(1.5f, -1.0f, 0f));
+            vertices.Add(new Vector3(1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(0f, -1.0f, -1.5f));
+            vertices.Add(new Vector3(-1.0f, -1.0f, -1.0f));
+            vertices.Add(new Vector3(-1.5f, -1.0f, 0f));
+            vertices.Add(new Vector3(-1.0f, -1.0f, 1.0f));
+            vertices.Add(new Vector3(0f, -1.0f, 1.5f));
+            polygons.Add(new List<int> { 8, 9, 10, 11, 12, 13, 14, 15 });
+
+            //topje
+            vertices.Add(new Vector3(0f, 3f, 0f));
+
+            //onderkant
+            vertices.Add(new Vector3(0f, -3f, 0f));
+            polygons.Add(new List<int> { 0, 16, 4, 12, 17, 8 });
+
+
+        }
+
+    }
+
+
 }
